c1e6236ece98895ed77fdcddbaee85fec7389dae
kongpda <kongpda@gmail.com>
2024-12-07 16:39:41 +0700

    refactor route from main.js to /router/index.js
    this refactor integrate with admin page and make all the route in /router/index.js

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <Header></Header>
-  <router-view></router-view>
-  <!-- <AboutUs></AboutUs> -->
-  <Footer></Footer>
+  <RouterView />
 </template>
-
-<script>
+<!-- <template>
+ <template v-if="$route.path.startsWith('/admin')">
+   <AdminLayout>
+      <RouterView />
+    </AdminLayout>
+  </template>
+  <template v-else>
+    <Header />
+    <main>
+      <RouterView />
+    </main>
+    <Footer />
+  </template>
+</template> -->
+<script setup>
+import { RouterView } from 'vue-router';
+import AdminLayout from "@/layouts/AdminLayout.vue";
 import Header from "@/components/Header.vue";
-// import AboutUs from "./components/AboutUs.vue";
 import Footer from "@/components/Footer.vue";
-export default {
-  name: "App",
-  components: {
-    Header,
-    // AboutUs,
-    Footer,
-  },
-};
 </script>
